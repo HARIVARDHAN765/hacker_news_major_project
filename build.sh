1e61b6475122da89e17481d521011eddd773f5f3
Hari Vardhan <your_email@example.com>
2026-06-30 21:06:03 +0530

    update

--- build.sh
+++ build.sh
@@ -6,7 +6,3 @@ pip install -r requirements.txt
 python manage.py collectstatic --noinput
 
 python manage.py migrate
-
-python manage.py search_index --create
-
-python manage.py search_index --populate
